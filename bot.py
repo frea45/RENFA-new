1deb792b3d55a19d9900ebcb6991c0e258c92737
frea45 <110092585+frea45@users.noreply.github.com>
2025-04-09 15:39:30 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,32 @@
-
-
 from pyrogram import Client, idle
 from plugins.cb_data import app as Client2
 from config import *
-
-
+import asyncio
 
 bot = Client(
+    "Renamer",
+    bot_token=BOT_TOKEN,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    plugins=dict(root='plugins')
+)
 
-           "Renamer",
+async def main():
+    if STRING:
+        apps = [Client2, bot]
+        for app in apps:
+            await app.start()
+        print("All Clients Started")
+        await idle()
+        for app in apps:
+            await app.stop()
+        print("All Clients Stopped")
+    else:
+        await bot.start()
+        print("Bot Started (No STRING)")
+        await idle()
+        await bot.stop()
+        print("Bot Stopped")
 
-           bot_token=BOT_TOKEN,
-
-           api_id=API_ID,
-
-           api_hash=API_HASH,
-
-           plugins=dict(root='plugins'))
-           
-
-if STRING:
-    apps = [Client2,bot]
-    for app in apps:
-        app.start()
-    idle()
-    for app in apps:
-        app.stop()
-    
-else:
-    bot.run()
+if __name__ == "__main__":
+    asyncio.run(main())
