272fca5e1102c682060dcdfdd398587c83f9263f
frea45 <110092585+frea45@users.noreply.github.com>
2025-12-01 15:10:51 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os, re
 
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "1996499923:AAGVCjT4msY1AOHb-qtalwMsrwu0dD65k7g")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7136875110:AAGr1EREy_qPMgxVbuE4B0cHGVcwWudOrus")
 API_ID = int(os.environ.get("API_ID", "3335796"))
 API_HASH = os.environ.get("API_HASH", "138b992a0e672e8346d8439c3f42ea78")
 OWNER = int(os.environ.get("OWNER", "763990585"))
-BOT_USERNAME = os.environ.get('BOT_USERNAME', "ir_renamebot")
+BOT_USERNAME = os.environ.get('BOT_USERNAME', "ir_YoutubeDlbot")
 
 FORCE_SUBS = os.environ.get("FORCE_SUBS", "ir_botz")
 LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001792962793"))
